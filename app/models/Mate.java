48f3699834a6fe1da1ff622dd618e9897324b4cc
akramk <akramk@in.tum.de>
2014-06-07 16:14:16 +0200

    Mate, Address, Seeker model created. Ready to Help button event implemented.

---
+++ app/models/Mate.java
@@ -0,0 +1,36 @@
+package models;
+
+import play.db.jpa.Model;
+
+
+public class Mate extends Model{
+	
+	public String ssid;
+	public String firstName;
+	public String lastName;
+	public int age;
+	public String email;
+	public String pass;
+	/**
+	 * @param ssid
+	 * @param firstName
+	 * @param lastName
+	 * @param age
+	 * @param email
+	 * @param pass
+	 */
+	public Mate(String ssid, String firstName, String lastName, int age,
+			String email, String pass) {
+		super();
+		this.ssid = ssid;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.email = email;
+		this.pass = pass;
+	}
+	
+	
+	
+	
+}
---
+++ app/models/Seeker.java
@@ -0,0 +1,39 @@
+package models;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+
+@Entity
+public class Seeker extends Model{
+	public String ssid;
+	public String firstName;
+	public String lastName;
+	public int age;
+	public String email;
+	public String pass;
+	
+	/**
+	 * @param ssid
+	 * @param firstName
+	 * @param lastName
+	 * @param age
+	 * @param email
+	 * @param pass
+	 */
+	public Seeker(String ssid, String firstName, String lastName, int age,
+			String email, String pass) {
+		super();
+		this.ssid = ssid;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.email = email;
+		this.pass = pass;
+	}
+	
+	
+	
+
+}
