e7bf1479cba673d6202dd78a741ea677d3d61253
ADNAN RABBI <adnanrabbi0825@gmail.com>
2014-06-07 16:21:13 +0200

    Log in html page

---
+++ app/controllers/LogInController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import java.awt.Panel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import play.mvc.Controller;
+
+public class LogInController extends Controller {
+
+	public static void logIn(){
+		render();
+    }
+}
